5cd501798699e363f0ee26455669dc229386369c
David Pickett <pickettd@gmail.com>
2017-05-07 20:22:22 -1000

    Update dist to remove unused plugins

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -10,20 +10,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
-        "id": "cordova-plugin-statusbar.StatusBarProxy",
-        "pluginId": "cordova-plugin-statusbar",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "pluginId": "cordova-plugin-device",
@@ -43,7 +29,6 @@ module.exports.metadata =
 {
     "ionic-plugin-keyboard": "2.2.1",
     "cordova-plugin-whitelist": "1.3.1",
-    "cordova-plugin-statusbar": "2.2.1",
     "cordova-plugin-device": "1.1.4"
 }
 // BOTTOM OF METADATA
